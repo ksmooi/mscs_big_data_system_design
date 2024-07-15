6efe2fa5d21ec8523a308b08b6ad8cc9ff5d6a87
ksmooi <kaishianmooi@gmail.com>
2024-07-15 12:06:54 +0800

    fixed hard code issue

--- src/api/api_server.py
+++ src/api/api_server.py
@@ -3,8 +3,21 @@
 # python src/api/api_server.py
 # deactivate
 
+import json
+import os
 from flask import Flask
 from api.routes import api_blueprint
+
+def load_config():
+    """
+    Loads the configuration from the config.json file.
+
+    Returns:
+        dict: Configuration dictionary.
+    """
+    config_path = os.path.join(os.path.dirname(__file__), '../../config/config.json')
+    with open(config_path, 'r') as config_file:
+        return json.load(config_file)
 
 def create_app():
     """
@@ -27,8 +40,9 @@ def run_api_server():
     This function creates the Flask application by calling create_app(),
     and then runs the application on the specified host and port.
     """
+    config = load_config()
     app = create_app()
-    app.run(host='0.0.0.0', port=5001, debug=False, use_reloader=False)
+    app.run(host=config['api_server']['host'], port=config['api_server']['port'], debug=config['api_server']['debug'], use_reloader=False)
 
 if __name__ == '__main__':
     """
